194141c13484d326e94268f7a28fc1d57f16fb5e
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-07-03 10:51:19 +0900

    DMモデルの修正

--- db/migrate/20230703014149_create_direct_messages.rb
+++ db/migrate/20230703014149_create_direct_messages.rb
@@ -2,7 +2,7 @@ class CreateDirectMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :direct_messages do |t|
       t.references :user, null: false, foreign_key: true
-      t.bigint :target_id
+      t.references :target, foreign_key: { to_table: :users }
 
       t.timestamps
     end
